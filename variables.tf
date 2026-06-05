b1a8e5c1d737ac288be24a49bef68e00270557cf
mohamedmagdy122 <your-email@gmail.com>
2026-06-05 05:33:57 +0300

    Add VPC module

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "name" {
+  description = "Name prefix for all resources"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
